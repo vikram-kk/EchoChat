5e7bbb9f7750645424a6d129237bb74209238e71
Vikram Thakur <vikramkk380@gmail.com>
2026-03-28 20:49:51 +0530

    ui improvement and new message first pop feature

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect, use } from "react";
+import { useState, useEffect, useRef } from "react";
 import socket from "../serives/socket.api";
-
+import logo from "./assets/logo.png";
 export default function Chat() {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
@@ -10,6 +10,24 @@ export default function Chat() {
   const [onlineUsers, setOnlineUsers] = useState([]);
   const [isTyping, setIsTyping] = useState(false);
   const [typing, setTyping] = useState("");
+
+  const chatContainerRef = useRef(null);
+  const messageEndRef = useRef(null);
+
+  const isNearBotton = () => {
+    const el = chatContainerRef.current;
+    return el.scrollHeight - el.scrollTop - el.clientHeight < 100;
+  };
+
+  const scrolltobottom = () => {
+    messageEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    if (isNearBotton()) {
+      scrolltobottom();
+    }
+  }, [messages]);
 
   useEffect(() => {
     socket.on("receiveMessage", (data) => {
@@ -24,12 +42,13 @@ export default function Chat() {
     socket.on("userTyping", (data) => {
       //   console.log(data);
       setTyping(data.sender);
-
       setIsTyping(true);
 
-      setTimeout(() => {
+      clearTimeout(typingTimeout);
+
+      typingTimeout = setTimeout(() => {
         setIsTyping(false);
-      }, 1800);
+      }, 1500);
     });
 
     socket.on("getOnlineUsers", (users) => {
@@ -108,24 +127,34 @@ export default function Chat() {
   }
 
   return (
-    <div className="bg-amber-600 h-screen w-full flex items-center justify-center">
-      <div className=" bg-amber-400 p-4 rounded shadow-2xl">
-        <div className="flex flex-col items-center justify-center">
-          <h2 className="font-bold text-2xl uppercase">
-            Real Time Messaging App
-          </h2>
-          <h3 className="font-semibold">Online Users:</h3>
-          <ul className="  text-amber-200  w-full">
-            {onlineUsers.map((user, index) => (
+    <div className="bg-gray-200  h-screen  w-full pt-2 px-2">
+      {/* heading */}
+      <div className="flex  items-center justify-between px-2 mb-2  rounded bg-white shadow-lg">
+        {/* //Name of project */}
+        <h2 className="font-bold text-2xl ">EchoChat</h2>
+        <img src={logo} width={64} className="" alt="Echochat" />
+      </div>
+
+      {/* two containers  */}
+
+      <div className="grid grid-cols-3 gap-4">
+        {/* online users list  */}
+        <div className="h-117 bg-white  rounded px-1 shadow-2xl">
+          <h3 className="font-semibold px-1 mb-2">Messages</h3>
+          <h3 className="border-b-1  px-1 text-[12px] text-orange-600 w-20 pb-2">
+            All messages
+          </h3>
+          <ul className="  text-black  w-full">
+            {onlineUsers.map((users, index) => (
               <li
                 key={index}
-                className="p-2 bg-amber-900 flex rounded justify-between m-1"
+                className={`py-2 px-1 flex  justify-between border-b-1 border-gray-500/50 ${users == user ? "hidden" : ""}`}
               >
-                {user}{" "}
+                {users}{" "}
                 <button
                   className="bg-amber-200 text-amber-900 hover:bg-green-600 hover:text-white transition-all ease-in px-4 py-0.5 rounded-2xl"
                   onClick={() => {
-                    joinRoom(user);
+                    joinRoom(users);
                   }}
                 >
                   {" "}
@@ -134,39 +163,56 @@ export default function Chat() {
               </li>
             ))}
           </ul>
+        </div>
+
+        {/* message container and list  */}
+        {/* //message list container */}
+        <div
+          className={`bg-gray-100 shadow-lg relative col-span-2 h-117 w-full pl-2 rounded no-scrollbar`}
+        >
           <div
-            className={`bg-amber-200 shadow-2xl overflow-auto h-70
-                 w-full pl-2 rounded border-2 border-amber-100 no-scrollbar`}
+            ref={chatContainerRef}
+            className="h-110 overflow-auto no-scrollbar"
           >
             {messages.map((item, index) => (
+              //messages list
               <div
-                className={`flex ${item.sender === user ? "flex-row-reverse" : "flex-row"}`}
+                className={`flex ${item.sender === user ? "flex-row-reverse" : "flex-row"} pb-5`}
                 key={index}
               >
-                <p className="bg-amber-50 m-1 max-w-30  p-2 flex flex-col rounded-3xl">
+                <p className="bg-orange-100/50 m-1 max-w-60   p-2 flex flex-col rounded-3xl">
                   <strong className="text-[11px]">{item.sender}:</strong>
                   <span className="text-sm">{item.message}</span>
                 </p>
               </div>
             ))}
+            <div ref={messageEndRef}></div>
           </div>
-        </div>
-        {(isTyping && <p>: {typing} is typing...</p>) || <p>:</p>}
-        <div className="flex justify-between">
-          <input
-            className=" bg-amber-100 w-full mr-2 px-2 rounded focus:outline-none py-0.5"
-            type="text"
-            placeholder="Type your message"
-            value={message}
-            onChange={(e) => handleTyping(e)}
-          />
-          <button
-            className="bg-amber-100 px-4 rounded hover:bg-amber-900 hover:text-white transition-all ease-in"
-            disabled={!user.trim()}
-            onClick={sendMessage}
-          >
-            Send
-          </button>
+
+          {/* typing indicator and message input  */}
+          <div>
+            <div className="absolute bottom-12">
+              {(isTyping && <h5 className="">: {typing} is typing...</h5>) || (
+                <h5>:</h5>
+              )}
+            </div>
+            <div className="flex justify-between w-full overflow-auto absolute bottom-2  pr-3 shadow-xl ">
+              <input
+                className=" bg-white w-full shadow-sm mr-2 px-2 rounded focus:outline-1 py-2"
+                type="text"
+                placeholder="Type your message"
+                value={message}
+                onChange={(e) => handleTyping(e)}
+              />
+              <button
+                className="bg-amber-100 px-4 rounded hover:bg-amber-900 hover:text-white transition-all ease-in"
+                disabled={!user.trim()}
+                onClick={sendMessage}
+              >
+                Send
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
